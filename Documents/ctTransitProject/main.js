aebdc44142ed8307634aef3a079c67bc1c75a363
Taylor Somma <taylor.somma@gmail.com>
2016-11-06 22:37:17 -0500

    Add sample route drawing, add design for route and bus popups.

--- Documents/ctTransitProject/main.js
+++ Documents/ctTransitProject/main.js
@@ -1,16 +1,24 @@
 function getBusLocations() {
   var busLocations = $.getJSON( "http://65.213.12.244/realtimefeed/vehicle/vehiclepositions.json", function() {
+
       busLocationsParse = JSON.parse(busLocations.responseText)
         console.log(busLocationsParse["entity"][0])
-        busLat = (busLocationsParse["entity"][0]["vehicle"]["position"]["latitude"])
-        busLong = (busLocationsParse["entity"][0]["vehicle"]["position"]["latitude"])
-        console.log("busLat: "+busLat)
-        for (bus in busLocationsParse["entity"]){
-           console.log("Latitude: "+busLocationsParse["entity"][bus]["vehicle"]["position"]["latitude"])
-           console.log("Longitude: "+busLocationsParse["entity"][bus]["vehicle"]["position"]["longitude"])
-        }
-      return ([busLat,busLong]);
+
+      busLat = (busLocationsParse["entity"][0]["vehicle"]["position"]["latitude"])
+      busLong = (busLocationsParse["entity"][0]["vehicle"]["position"]["latitude"])
+      console.log("busLat: "+busLat)
+
+      for (bus in busLocationsParse["entity"]){
+         console.log("Latitude: "+busLocationsParse["entity"][bus]["vehicle"]["position"]["latitude"])
+         console.log("Longitude: "+busLocationsParse["entity"][bus]["vehicle"]["position"]["longitude"])
+    }
+    return ([busLat,busLong]);
   })
+}
+
+function getRoutes() {
+  var routeShapes = $.getJSON("googlha_transit/shapes.txt")
+  var routes = $.getJSON("googlha_transit/routes.txt")
 }
 
 //var testBusLoc = getBusLocations()
--- Documents/ctTransitProject/map.js
+++ Documents/ctTransitProject/map.js
@@ -10,10 +10,30 @@ function initMap() {
     }
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 9,
+      zoom: 15,
       center: {lat: 41.7637, lng: -72.6851}
     });
     var uluru = {lat: 41.7637, lng: -72.6851};
+
+    var shapeCoords = [
+    {lat: 41.764092,lng: -72.673866},
+    {lat: 41.765766,lng: -72.673357},
+    {lat: 41.765968,lng: -72.673294},
+    {lat: 41.766371,lng: -72.673177},
+    {lat: 41.766467,lng: -72.673165}
+
+    ];
+
+    var routePath = new google.maps.Polyline({
+          path: shapeCoords,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+    routePath.setMap(map);
+
     var marker = new google.maps.Marker({
       position: uluru,
       icon: icons["busIcons"].icon,
